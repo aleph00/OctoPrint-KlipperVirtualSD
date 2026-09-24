38050ed35d039ac0484b01adeeb4f37a34c56589
aleph00 <gf.nospam@free.fr>
2026-09-25 00:03:33 +0200

    Bump plugin version to 0.9.0

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 
 setup(
     name="OctoPrint-KlipperVirtualSD",
-    version="0.8.1",
+    version="0.9.0",
     packages=["octoprint_klippervirtualsd"],
-    package_data={"octoprint_klippervirtualsd": ["static/js/*.js"]},
+    package_data={
+        "octoprint_klippervirtualsd": [
+            "static/js/*.js",
+            "templates/*.jinja2",
+        ]
+    },
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.7",
